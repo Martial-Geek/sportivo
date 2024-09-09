91a4b0d12660c3851dbaa53d10bcd05c69d0864a
Uwais Jawed <uwaisjawed8081@gmail.com>
2024-09-09 23:19:10 +0530

    Add loading state

--- components/LandingPage.jsx
+++ components/LandingPage.jsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useEffect, useState } from "react";
+
 import { fetchImages } from "@/utils/getFirebaseImagePath";
 import {
   Navigation,
@@ -16,60 +17,120 @@ import "swiper/css/scrollbar";
 
 import Image from "next/image";
 import "../app/plainstyles.css";
+import { Skeleton } from "@mui/material";
 
 const LandingPage = () => {
   const [slides, setSlides] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     (async () => {
       const urls = await fetchImages("images/slide");
       setSlides(urls);
+      setLoading(false);
     })();
   }, []);
 
   return (
     <section className="home" id="home">
-      <div className="mb-28 mt-40 w-full">
-        <h1 className="my-14 text-center font-serif text-[40px] font-bold leading-tight text-[#3867d6] sm:text-[80px]">
-          Welcome to Team Sportivo
-        </h1>
-        <h3 className="w-auto px-12 text-center font-serif text-[20px] leading-[30px] text-white sm:p-0 sm:text-[50px] sm:leading-[60px]">
-          The Official Sports Club of Future Institute Of Engineering and
-          Management
-        </h3>
-      </div>
-
-      <Swiper
-        modules={[Navigation, Pagination, Scrollbar, A11y, Autoplay]}
-        autoplay={{ delay: 3000, disableOnInteraction: false }}
-        spaceBetween={55}
-        slidesPerView={3}
-        navigation
-        pagination={{ clickable: true }}
-        breakpoints={{
-          // when window width is >= 768px
-          768: {
-            slidesPerView: 3,
-          },
-          // when window width is < 768px
-          0: {
-            slidesPerView: 1,
-          },
-        }}
-      >
-        {slides.map((slide, index) => (
-          <SwiperSlide key={index}>
-            <div className="relative mx-auto size-[300px] shadow-xl sm:size-[350px]">
-              <Image
-                src={slide}
-                alt={`Image ${index}`}
-                fill
-                className="rounded-lg object-cover"
+      {loading ? (
+        <>
+          <div className="mt-64 flex w-full flex-col gap-20 max-md:hidden">
+            <Skeleton
+              variant="rectangular"
+              height={100}
+              className="rounded-xl"
+            />
+            <Skeleton
+              variant="rectangular"
+              height={50}
+              className="rounded-xl"
+            />
+            <div className="flex gap-20">
+              <Skeleton
+                variant="rectangular"
+                height={300}
+                animation="wave"
+                className="basis-1/3 rounded-xl"
+              />
+              <Skeleton
+                variant="rectangular"
+                height={300}
+                animation="wave"
+                className="basis-1/3 rounded-xl"
+              />
+              <Skeleton
+                variant="rectangular"
+                height={300}
+                animation="wave"
+                className="basis-1/3 rounded-xl"
               />
             </div>
-          </SwiperSlide>
-        ))}
-      </Swiper>
+          </div>
+
+          <div className="mt-48 flex w-full flex-col gap-20 md:hidden">
+            <Skeleton
+              variant="rectangular"
+              height={80}
+              className="rounded-xl"
+            />
+            <Skeleton
+              variant="rectangular"
+              height={40}
+              className="rounded-xl"
+            />
+            <Skeleton
+              variant="rectangular"
+              height={260}
+              animation="wave"
+              className="rounded-xl"
+            />
+          </div>
+        </>
+      ) : (
+        <>
+          <div className="mb-28 mt-40 w-full">
+            <h1 className="my-14 text-center font-serif text-[40px] font-bold leading-tight text-[#3867d6] sm:text-[80px]">
+              Welcome to Team Sportivo
+            </h1>
+            <h3 className="w-auto px-12 text-center font-serif text-[20px] leading-[30px] text-white sm:p-0 sm:text-[50px] sm:leading-[60px]">
+              The Official Sports Club of Future Institute Of Engineering and
+              Management
+            </h3>
+          </div>
+          <Swiper
+            modules={[Navigation, Pagination, Scrollbar, A11y, Autoplay]}
+            autoplay={{ delay: 3000, disableOnInteraction: false }}
+            spaceBetween={55}
+            slidesPerView={3}
+            navigation
+            pagination={{ clickable: true }}
+            breakpoints={{
+              // when window width is >= 768px
+              768: {
+                slidesPerView: 3,
+              },
+              // when window width is < 768px
+              0: {
+                slidesPerView: 1,
+              },
+            }}
+          >
+            {slides.map((slide, index) => (
+              <SwiperSlide key={index}>
+                <div className="relative mx-auto size-[300px] shadow-xl sm:size-[350px]">
+                  <Image
+                    src={slide}
+                    alt={`Image ${index}`}
+                    fill
+                    className="rounded-lg object-cover"
+                  />
+                </div>
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </>
+      )}
     </section>
   );
 };
